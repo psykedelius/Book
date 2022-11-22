a6277ef9e4653ca2baaff6529ef6dc51b723b745
psykedelius <34650265+psykedelius@users.noreply.github.com>
2022-11-22 02:34:16 +0100

    working 2D physics for the player

--- game/js/game.js
+++ game/js/game.js
@@ -48,10 +48,4 @@ function draw() {
 
 }
 
-    function checkIntersection(r1,r2){
-      if (r1.x >= r2.x + r2.width){ return false;}
-      else if (r1.x + r1.width <= r2.x){ return false;}
-      else if (r1.y >= r2.y + r2.height){ return false;}
-      else if (r1.y + r1.height <= r2.y){ return false;}
-      else { return true;}
-    }
+
